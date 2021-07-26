ff4a4346437c103b8709953f2e14849e1913aba4
Vi <engrycat@gmail.com>
2021-07-26 20:11:58 +0300

    add tab js + list content

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1200,
 		prevArrow: '<button type="button" class="slick-prev"><img src="img/ico/left_solid.svg"/></button>',
@@ -10,8 +10,26 @@ $(document).ready(function(){
 					dots: true,
 					dotsClass: 'slick-dots',
 					arrows: false,
-						}
+				}
 			}
 		]
 	});
-  });
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				console.log("3")
+				e.preventDefault();
+				$(".catalog-item__content").eq(i).toggleClass('catalog-item__content_active');
+				$(".catalog-item__list").eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	};
+	toggleSlide(".catalog-item__link");
+	toggleSlide(".catalog-item__back");
+});
